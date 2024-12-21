9a5b54abd79d752c284a8684c4e744c74123213b
GabrielZegarraM <gabrielzegarram@gmail.com>
2024-12-21 12:41:35 -0300

    Criando paginação da home

--- lib/modules/home/presentation/cubit/home_cubit.dart
+++ lib/modules/home/presentation/cubit/home_cubit.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
+
 import '../../../../shared/utils/dependencies/dependencies_export.dart';
 import '../../domain/usecases/home_usecase.dart';
 import 'states/home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeUseCase useCase;
+  final ScrollController scrollController = ScrollController();
 
   HomeCubit(this.useCase) : super(HomeState().initial());
 
@@ -26,7 +29,63 @@ class HomeCubit extends Cubit<HomeState> {
             success: true,
           ),
         );
+        paginate();
       },
     );
   }
+
+  void paginate({int? page}) {
+    final currentPage = page ?? state.currentPage;
+    final startIndex = (currentPage - 1) * state.pageSize;
+    final endIndex = startIndex + state.pageSize;
+
+    final displayedEntities = state.districtEntity.sublist(
+      startIndex,
+      endIndex > state.districtEntity.length
+          ? state.districtEntity.length
+          : endIndex,
+    );
+
+    emit(state.copyWith(
+      currentPage: currentPage,
+      displayedEntities: displayedEntities,
+    ));
+  }
+
+  void loadMore() {
+    if (state.isLoadingMore || state.displayedEntities.length == state.districtEntity.length) {
+      return;
+    }
+
+    emit(state.copyWith(isLoadingMore: true));
+
+    final nextPage = state.currentPage + 1;
+    final startIndex = (nextPage - 1) * state.pageSize;
+    final endIndex = startIndex + state.pageSize;
+
+    final moreItems = state.districtEntity.sublist(
+      startIndex,
+      endIndex > state.districtEntity.length ? state.districtEntity.length : endIndex,
+    );
+
+    emit(state.copyWith(
+      isLoadingMore: false,
+      currentPage: nextPage,
+      displayedEntities: List.from(state.displayedEntities)..addAll(moreItems),
+    ));
+  }
+
+  void _onScroll() {
+    if (scrollController.position.pixels >= scrollController.position.maxScrollExtent &&
+        !state.isLoadingMore) {
+      loadMore();
+    }
+  }
+
+  @override
+  Future<void> close() {
+    scrollController.dispose();
+    return super.close();
+  }
+
 }
--- lib/modules/home/presentation/cubit/states/home_state.dart
+++ lib/modules/home/presentation/cubit/states/home_state.dart
@@ -2,53 +2,76 @@ import '../../../../../shared/utils/states/global_state.dart';
 import '../../../domain/entities/locations_entity.dart';
 
 class HomeState implements GlobalState {
-  final List<DistrictEntity>? districtEntity;
+  final List<DistrictEntity> districtEntity;
+  final bool isLoadingMore;
   bool isLoading;
   bool success;
   bool successUpload;
   final String errorMessage;
+  final List<DistrictEntity> displayedEntities;
+  final int currentPage;
+  final int pageSize;
 
   HomeState({
     this.districtEntity = const [],
+    this.isLoadingMore = false,
     this.isLoading = false,
     this.success = false,
     this.successUpload = false,
     this.errorMessage = '',
+    this.displayedEntities = const [],
+    this.currentPage = 1,
+    this.pageSize = 10,
   });
 
   HomeState copyWith({
     List<DistrictEntity>? districtEntity,
+    bool? isLoadingMore,
     bool? isLoading,
     bool? success,
     bool? successUpload,
     String? errorMessage,
+    List<DistrictEntity>? displayedEntities,
+    int? currentPage,
+    int? pageSize,
   }) {
     return HomeState(
       districtEntity: districtEntity ?? this.districtEntity,
+      isLoadingMore: isLoadingMore ?? this.isLoadingMore,
       isLoading: isLoading ?? this.isLoading,
       success: success ?? this.success,
       successUpload: successUpload ?? this.successUpload,
       errorMessage: errorMessage ?? this.errorMessage,
+      displayedEntities: displayedEntities ?? this.displayedEntities,
+      currentPage: currentPage ?? this.currentPage,
+      pageSize: pageSize ?? this.pageSize,
     );
   }
 
   HomeState initial() {
     return HomeState(
       districtEntity: [],
+      isLoadingMore: false,
       isLoading: false,
       success: false,
       successUpload: false,
       errorMessage: '',
+      displayedEntities: [],
+      currentPage: 1,
+      pageSize: 10,
     );
   }
 
   HomeState loading() {
     return HomeState(
       isLoading: true,
+      isLoadingMore: false,
       success: success,
       districtEntity: [],
       successUpload: successUpload,
       errorMessage: '',
+      currentPage: 1,
+      pageSize: 10,
     );
   }
 }
